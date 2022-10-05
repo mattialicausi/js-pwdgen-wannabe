59cc77395e2daab53ee5aebf44ee2348b8bad4ec
mattialicausi <licausivitomattia@gmail.com>
2022-10-05 14:28:31 +0200

    aggiunto codice javascript per ottenere il risultato richiesto nella consegna

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@
 // poi chiedi la sua età
 // Infine scrivi sulla pagina nomecognomecolorepreferito21 (dove il 21 è in questo caso l'età)
 
+
+let nomeUtente = prompt ("Qual'è il tuo nome?");
+console.log (nomeUtente);
+
+let cognomeUtente = prompt ("Qual'è il tuo cognome?");
+console.log (cognomeUtente);
+
+let colorePreferito = prompt ("Qual'è il tuo colore preferito?");
+console.log (colorePreferito);
+
+let etaUtente = prompt ("Quanti anni hai?");
+console.log (etaUtente);
+
+const somma =
+`
+${nomeUtente}${cognomeUtente}${colorePreferito}${etaUtente}
+`;
+console.log (somma);
+
+const elemento = document.getElementById('risultato');
+
+elemento.innerHTML = somma;
+
+
+
