c16f5129327de419e024c810ae8c8c0be5078fc4
mattialicausi <licausivitomattia@gmail.com>
2022-10-05 13:32:58 +0200

    preparazione repo

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// poi chiedi la sua età
+// Infine scrivi sulla pagina nomecognomecolorepreferito21 (dove il 21 è in questo caso l'età)
+
